12cc87d7d4d1f1bfc215e2d3ea48724fec84d40c
Ledell Wu <ledell@fb.com>
2019-01-25 13:35:55 -0800

    Updated script on fb15k
    Summary: Providing a python script to run the example on fb15k end to end. It creates a directory /examples where the scripts and configs for public examples are under this directory.
    
    Reviewed By: lerks
    
    Differential Revision: D13781080
    
    fbshipit-source-id: 86a98572814f11f9a8f57a84fde3a8e63ae10894

---
+++ examples/configs/fb15k_config.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+entity_base = "data/FB15k"
+
+
+def getConf():
+
+    config = dict(
+        entityPath=entity_base,
+
+        numEpochs=200,
+
+        entities={
+            'all': {'numPartitions': 1},
+        },
+
+        relations=[{
+            'name': 'all_edges',
+            'lhs': 'all',
+            'rhs': 'all',
+            'operator': 'translation'
+        }],
+
+        edgePaths=[],
+
+        model='fb15k_dynamic',
+        outdir='model/fb15k',
+
+        dimension=100,
+        globalEmb=0,
+        maxNorm=1,
+        margin=0.2,
+        metric='dot',
+        lr=0.1,
+        numUniformNegs=0,
+        numBatchNegs=100,
+    )
+
+    return config
---
+++ examples/fb15k.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import argparse
+import os
+from itertools import chain
+
+import attr
+
+import torchbiggraph.converters.utils as utils
+from torchbiggraph.config import parse_config
+from torchbiggraph.converters.input_to_filament import convert_input_data
+from torchbiggraph.eval import do_eval
+from torchbiggraph.train import train
+
+
+FB15K_URL = 'https://dl.fbaipublicfiles.com/starspace/fb15k.tgz'
+FILENAMES = {
+    'train': 'FB15k/freebase_mtr100_mte100-train.txt',
+    'valid': 'FB15k/freebase_mtr100_mte100-valid.txt',
+    'test': 'FB15k/freebase_mtr100_mte100-test.txt',
+}
+
+
+def convert_path(fname):
+    basename, _ = os.path.splitext(fname)
+    out_dir = basename + '_partitioned'
+    return out_dir
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Example on FB15k')
+    parser.add_argument('--config', default='examples/configs/fb15k_config.py',
+                        help='Path to config file')
+    parser.add_argument('-p', '--param', action='append', nargs='*')
+    parser.add_argument('--data_dir', default='data',
+                        help='where to save processed data')
+
+    args = parser.parse_args()
+
+    if args.param is not None:
+        overrides = chain.from_iterable(args.param)  # flatten
+    else:
+        overrides = None
+
+    # download data
+    data_dir = args.data_dir
+    fpath = utils.download_url(FB15K_URL, data_dir)
+    utils.extract_tar(fpath)
+    print('Downloaded and extracted file.')
+
+    edgePaths = [os.path.join(data_dir, name) for name in FILENAMES.values()]
+    convert_input_data(
+        args.config,
+        edgePaths,
+        isDynamic=1,
+        srcCol=0,
+        destCol=2,
+        relationCol=1,
+    )
+
+    config = parse_config(args.config, overrides)
+
+    trainPath = [convert_path(os.path.join(data_dir, FILENAMES['train']))]
+    train_config = attr.evolve(config, edgePaths=trainPath)
+
+    train(train_config)
+
+    evalPath = [convert_path(os.path.join(data_dir, FILENAMES['test']))]
+    relations = [attr.evolve(r, all_rhs_negs=1) for r in config.relations]
+    eval_config = attr.evolve(config, edgePaths=evalPath, relations=relations)
+
+    do_eval(eval_config)
+
+
+if __name__ == "__main__":
+    main()
