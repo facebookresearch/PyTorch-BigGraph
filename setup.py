dda4e4d6c8937ba4af04e8799eb32f63aba6b7bc
Luca Wehrstedt <lcw@fb.com>
2019-01-30 09:40:41 -0800

    Add missing license headers
    Summary: We forgot them in recently-added files
    
    Reviewed By: adamlerer
    
    Differential Revision: D13879474
    
    fbshipit-source-id: 9a84ad82b15987b42ff105d4d16214b99f7f8644

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 from setuptools import setup, find_packages
 
 with open("README.md", "rt") as f:
