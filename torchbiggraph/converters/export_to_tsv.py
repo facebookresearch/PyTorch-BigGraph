d4c6dc4ca481ba9ef6c79cc773ab56cdb7340a44
simonepri <simonepri@outlook.com>
2019-06-25 13:39:49 +0200

    Fix codestyle

--- torchbiggraph/converters/export_to_tsv.py
+++ torchbiggraph/converters/export_to_tsv.py
@@ -24,8 +24,8 @@ def make_tsv(
     checkpoint: str,
     relation_types: List[str],
     entities_by_type: Dict[str, List[str]],
-    ent_out_file: IO[str],
-    rel_out_file: IO[str],
+    entities_output_file: IO[str],
+    relation_types_output_file: IO[str],
 ) -> None:
     print("Loading model check point...")
     checkpoint_manager = CheckpointManager(checkpoint)
@@ -47,14 +47,15 @@ def make_tsv(
                 embs += model.global_embs[model.EMB_PREFIX + entity_name]
 
             for ix in range(len(embs)):
-                write(ent_out_file, entities[part_begin + ix], embs[ix])
+                write(entities_output_file, entities[part_begin + ix], embs[ix])
                 if (ix + 1) % 5000 == 0:
                     print("- Processed %d entities so far..." % (ix + 1))
             print("- Processed %d entities in total" % len(embs))
 
             part_begin = part_begin + len(embs)
 
-    print("Done exporting entities data to %s" % getattr(ent_out_file, "name", "the output file"))
+    entities_output_filename = getattr(entities_output_file, "name", "the output file")
+    print("Done exporting entities data to %s" % entities_output_filename)
 
     # TODO Provide a better output format for relation parameters.
     print("Writing relation parameters...")
@@ -64,29 +65,30 @@ def make_tsv(
             for parameter in model.lhs_operators[0].parameters()
         ], dim=1)
         for rel_idx, rel_name in enumerate(relation_types):
-            write(rel_out_file, rel_name, lhs_parameters[rel_idx])
+            write(relation_types_output_file, rel_name, lhs_parameters[rel_idx])
 
         rhs_parameters = torch.cat([
             parameter.view(model.num_dynamic_rels, -1)
             for parameter in model.rhs_operators[0].parameters()
         ], dim=1)
         for rel_idx, rel_name in enumerate(relation_types):
-            write(rel_out_file, rel_name + "_reverse_relation", rhs_parameters[rel_idx])
+            write(relation_types_output_file, rel_name + "_reverse_relation", rhs_parameters[rel_idx])
     else:
         for rel_name, operator in zip(relation_types, model.rhs_operators):
-            write(rel_out_file, rel_name, torch.cat([
+            write(relation_types_output_file, rel_name, torch.cat([
                 parameter.flatten() for parameter in operator.parameters()
             ], dim=0))
 
-    print("Done exporting relations data to %s" % getattr(rel_out_file, "name", "the output file"))
+    relation_types_output_filename = getattr(relation_types_output_file, "name", "the output file"))
+    print("Done exporting relations data to %s" % relation_types_output_filename)
 
 
 def main():
     parser = argparse.ArgumentParser(description='Convert Data for PBG')
     parser.add_argument('--checkpoint')
     parser.add_argument('--dict', required=True)
-    parser.add_argument('--out-ent', required=True)
-    parser.add_argument('--out-rel', required=True)
+    parser.add_argument('--entities-output', required=True)
+    parser.add_argument('--relation-types-output', required=True)
 
     args = parser.parse_args()
 
