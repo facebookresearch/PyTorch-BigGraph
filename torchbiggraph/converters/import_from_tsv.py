facebookresearch/PyTorch-BigGraph

commit af783df122f1be881052f9f4023452be980b03cc

Make sure configs can be loaded in subprocesses
Summary: This was broken by D16600947 because it relied on fork preserving the parent environment, but this commit introduces a more resilient approach, which uses the plumbing introduced in D16581941.

Reviewed By: adamlerer

Differential Revision: D16600944

fbshipit-source-id: d16fd6d0ebf604126cf7e0a416cb7f80a6168a92

torchbiggraph/converters/import_from_tsv.py
@@ -11,16 +11,16 @@ import json
 import os
 import os.path
 import random
-from typing import Counter, DefaultDict, Dict, List, Optional, Tuple
+from typing import Any, Counter, DefaultDict, Dict, List, Optional, Tuple
 
 import h5py
 import numpy as np
 
 from torchbiggraph.config import (
+    ConfigFileLoader,
     ConfigSchema,
     EntitySchema,
     RelationSchema,
-    get_config_dict_from_module,
 )
 from torchbiggraph.converters.dictionary import Dictionary
 
@@ -324,16 +324,13 @@ def convert_input_data(
         )
 
 
-def validate_config(
-    config: str,
+def parse_config_partial(
+    config_dict: Any,
 ) -> Tuple[Dict[str, EntitySchema], List[RelationSchema], str, bool]:
-    user_config = get_config_dict_from_module(config)
-
-    # validate entites and relations config
-    entities_config = user_config.get("entities")
-    relations_config = user_config.get("relations")
-    entity_path = user_config.get("entity_path")
-    dynamic_relations = user_config.get("dynamic_relations", False)
+    entities_config = config_dict.get("entities")
+    relations_config = config_dict.get("relations")
+    entity_path = config_dict.get("entity_path")
+    dynamic_relations = config_dict.get("dynamic_relations", False)
     if not isinstance(entities_config, dict):
         raise TypeError("Config entities is not of type dict")
     if any(not isinstance(k, str) for k in entities_config.keys()):
@@ -374,11 +371,13 @@ def main():
                         help='Min count for relation types')
     parser.add_argument('--entity-min-count', type=int, default=1,
                         help='Min count for entities')
-
     opt = parser.parse_args()
 
+    loader = ConfigFileLoader()
+    config_dict = loader.load_raw_config(opt.config)
+
     entity_configs, relation_configs, entity_path, dynamic_relations = \
-        validate_config(opt.config)
+        parse_config_partial(config_dict)
 
     convert_input_data(
         entity_configs,

torchbiggraph/config.py
@@ -7,8 +7,11 @@
 # LICENSE.txt file in the root directory of this source tree.
 
 import argparse
-import importlib.util
+import importlib
+import os.path
+import shutil
 import sys
+import tempfile
 import uuid
 from enum import Enum
 from itertools import chain
@@ -358,28 +361,6 @@ class ConfigSchema(Schema):
             print("WARNING: You have logistic loss and cosine distance. Are you sure?")
 
 
-def get_config_dict_from_module(config_filename: str) -> Any:
-    module_name = f"torchbiggraph_config_{uuid.uuid4().hex}"
-    spec = importlib.util.spec_from_file_location(module_name, config_filename)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    # This is a nasty hack which is needed for models which contain custom
-    # operators, comparators, loss function, ... to be able to send them to the
-    # worker processes. This is because the model gets pickled when transferred
-    # and pickle expects to find all the objects in the module they declare to
-    # live in, and expects all modules to be importable from their name. In the
-    # workers the config module cannot automatically be imported by pickle as it
-    # doesn't know its path. However, we can import and register it ourselves,
-    # and then pickle will just use it. Or, since the workers are forked off the
-    # main process, we can import and register the module there and the children
-    # will just inherit it.
-    sys.modules[module_name] = module
-    config_dict = module.get_torchbiggraph_config()
-    if config_dict is None:
-        raise RuntimeError("Config module %s didn't return anything" % config_filename)
-    return config_dict
-
-
 # TODO make this a non-inplace operation
 def override_config_dict(config_dict: Any, overrides: List[str]) -> Any:
     for override in overrides:
@@ -403,10 +384,7 @@ def override_config_dict(config_dict: Any, overrides: List[str]) -> Any:
     return config_dict
 
 
-def parse_config(config_filename: str, overrides: Optional[List[str]] = None) -> ConfigSchema:
-    config_dict = get_config_dict_from_module(config_filename)
-    if overrides is not None:
-        config_dict = override_config_dict(config_dict, overrides)
+def parse_config(config_dict: Any) -> ConfigSchema:
     try:
         config = ConfigSchema.from_dict(config_dict)
     except DeepTypeError as err:
@@ -417,6 +395,56 @@ def parse_config(config_filename: str, overrides: Optional[List[str]] = None) ->
     from torchbiggraph import util
     util._verbosity_level = config.verbose
     return config
+
+
+class ConfigFileLoader:
+    """Load configs from source files, after setting them up as modules.
+
+    In order to support configs defined in Python files whose paths are passed
+    on the command line, we need to first load those files as modules (using
+    eval is for savages). If those files define classes or functions that need
+    to be pickled to be sent to the workers (say, custom operators, comparators,
+    ...) then their modules need to be "normally" importable: their filename
+    must match their module name and they must reside in a standard location
+    (i.e., a directory in the path).
+
+    All the above is taken care of by this class, which creates a temporary
+    directory in which to copy over the configs, with unique names, and then
+    imports them from there.
+    """
+
+    def __init__(self) -> None:
+        self.config_dir = tempfile.TemporaryDirectory(prefix="torchbiggraph_config_")
+        # Hold a reference because at destruction time it may not be available anymore.
+        self.sys_path = sys.path
+        self.sys_path.append(self.config_dir.name)
+
+    def __del__(self) -> None:
+        self.sys_path.remove(self.config_dir.name)
+        self.config_dir.cleanup()
+
+    def load_raw_config(self, path: str) -> Any:
+        module_name = f"torchbiggraph_config_{uuid.uuid4().hex}"
+        shutil.copyfile(path, os.path.join(self.config_dir.name, f"{module_name}.py"))
+        importlib.invalidate_caches()
+        module = importlib.import_module(module_name)
+        raw_config = module.get_torchbiggraph_config()
+        return raw_config
+
+    def load_config(
+        self,
+        path: str,
+        overrides: Optional[List[str]] = None,
+    ) -> ConfigSchema:
+        config_dict = self.load_raw_config(path)
+        if overrides is not None:
+            config_dict = override_config_dict(config_dict, overrides)
+        config = parse_config(config_dict)
+        return config
+
+
+def add_to_sys_path(path: str) -> None:
+    sys.path.append(path)
 
 
 def main():
@@ -430,7 +458,8 @@ def main():
         overrides = chain.from_iterable(opt.param)  # flatten
     else:
         overrides = None
-    config = parse_config(opt.config, overrides)
+    loader = ConfigFileLoader()
+    config = loader.load_config(opt.config, overrides)
 
     print(config[opt.query])
 

torchbiggraph/eval.py
@@ -22,7 +22,7 @@ from torchbiggraph.batching import (
 from torchbiggraph.bucket_scheduling import (
     create_buckets_ordered_lexicographically
 )
-from torchbiggraph.config import ConfigSchema, parse_config
+from torchbiggraph.config import add_to_sys_path, ConfigFileLoader, ConfigSchema
 from torchbiggraph.edgelist import EdgeList
 from torchbiggraph.fileio import CheckpointManager, EdgeReader
 from torchbiggraph.model import MultiRelationEmbedder, Scores, make_model
@@ -223,9 +223,10 @@ def main():
         overrides = chain.from_iterable(opt.param)  # flatten
     else:
         overrides = None
-    config = parse_config(opt.config, overrides)
+    loader = ConfigFileLoader()
+    config = loader.load_config(opt.config, overrides)
 
-    do_eval(config)
+    do_eval(config, subprocess_init=partial(add_to_sys_path, loader.config_dir.name))
 
 
 if __name__ == '__main__':

torchbiggraph/examples/fb15k.py
@@ -8,13 +8,14 @@
 
 import argparse
 import os
+from functools import partial
 from itertools import chain
 
 import attr
 import pkg_resources
 
 import torchbiggraph.converters.utils as utils
-from torchbiggraph.config import parse_config
+from torchbiggraph.config import add_to_sys_path, ConfigFileLoader
 from torchbiggraph.converters.import_from_tsv import convert_input_data
 from torchbiggraph.eval import do_eval
 from torchbiggraph.filtered_eval import FilteredRankingEvaluator
@@ -61,7 +62,8 @@ def main():
     utils.extract_tar(fpath)
     print('Downloaded and extracted file.')
 
-    config = parse_config(args.config, overrides)
+    loader = ConfigFileLoader()
+    config = loader.load_config(args.config, overrides)
     edge_paths = [os.path.join(data_dir, name) for name in FILENAMES.values()]
 
     convert_input_data(
@@ -78,7 +80,10 @@ def main():
     train_path = [convert_path(os.path.join(data_dir, FILENAMES['train']))]
     train_config = attr.evolve(config, edge_paths=train_path)
 
-    train(train_config)
+    train(
+        train_config,
+        subprocess_init=partial(add_to_sys_path, loader.config_dir.name),
+    )
 
     eval_path = [convert_path(os.path.join(data_dir, FILENAMES['test']))]
     relations = [attr.evolve(r, all_negs=True) for r in config.relations]
@@ -89,10 +94,16 @@ def main():
             convert_path(os.path.join(data_dir, FILENAMES['valid'])),
             convert_path(os.path.join(data_dir, FILENAMES['train'])),
         ]
-        do_eval(eval_config,
-                evaluator=FilteredRankingEvaluator(eval_config, filter_paths))
+        do_eval(
+            eval_config,
+            evaluator=FilteredRankingEvaluator(eval_config, filter_paths),
+            subprocess_init=partial(add_to_sys_path, loader.config_dir.name),
+        )
     else:
-        do_eval(eval_config)
+        do_eval(
+            eval_config,
+            subprocess_init=partial(add_to_sys_path, loader.config_dir.name),
+        )
 
 
 if __name__ == "__main__":

torchbiggraph/examples/livejournal.py
@@ -9,13 +9,14 @@
 import argparse
 import os
 import random
+from functools import partial
 from itertools import chain
 
 import attr
 import pkg_resources
 
 import torchbiggraph.converters.utils as utils
-from torchbiggraph.config import parse_config
+from torchbiggraph.config import add_to_sys_path, ConfigFileLoader
 from torchbiggraph.converters.import_from_tsv import convert_input_data
 from torchbiggraph.eval import do_eval
 from torchbiggraph.train import train
@@ -101,7 +102,8 @@ def main():
     # random split file for train and test
     random_split_file(fpath)
 
-    config = parse_config(args.config, overrides)
+    loader = ConfigFileLoader()
+    config = loader.load_config(args.config, overrides)
     edge_paths = [os.path.join(data_dir, name) for name in FILENAMES.values()]
 
     convert_input_data(
@@ -118,12 +120,18 @@ def main():
     train_path = [convert_path(os.path.join(data_dir, FILENAMES['train']))]
     train_config = attr.evolve(config, edge_paths=train_path)
 
-    train(train_config)
+    train(
+        train_config,
+        subprocess_init=partial(add_to_sys_path, loader.config_dir.name),
+    )
 
     eval_path = [convert_path(os.path.join(data_dir, FILENAMES['test']))]
     eval_config = attr.evolve(config, edge_paths=eval_path)
 
-    do_eval(eval_config)
+    do_eval(
+        eval_config,
+        subprocess_init=partial(add_to_sys_path, loader.config_dir.name),
+    )
 
 
 if __name__ == "__main__":

torchbiggraph/partitionserver.py
@@ -7,12 +7,13 @@
 # LICENSE.txt file in the root directory of this source tree.
 
 import argparse
+from functools import partial
 from itertools import chain
 from typing import Callable, Optional
 
 import torch.distributed as td
 
-from torchbiggraph.config import ConfigSchema, parse_config
+from torchbiggraph.config import add_to_sys_path, ConfigFileLoader, ConfigSchema
 from torchbiggraph.distributed import ProcessRanks, init_process_group
 from torchbiggraph.parameter_sharing import ParameterServer
 from torchbiggraph.types import Rank
@@ -68,9 +69,14 @@ def main():
         overrides = chain.from_iterable(opt.param)  # flatten
     else:
         overrides = None
-    config = parse_config(opt.config, overrides)
+    loader = ConfigFileLoader()
+    config = loader.load_config(opt.config, overrides)
 
-    run_partition_server(config, opt.rank)
+    run_partition_server(
+        config,
+        opt.rank,
+        subprocess_init=partial(add_to_sys_path, loader.config_dir.name),
+    )
 
 
 if __name__ == '__main__':

torchbiggraph/train.py
@@ -31,9 +31,10 @@ from torchbiggraph.bucket_scheduling import (
     SingleMachineBucketScheduler,
 )
 from torchbiggraph.config import (
+    add_to_sys_path,
+    ConfigFileLoader,
     ConfigSchema,
     RelationSchema,
-    parse_config,
 )
 from torchbiggraph.distributed import (
     ProcessRanks,
@@ -820,9 +821,14 @@ def main():
         overrides = chain.from_iterable(opt.param)  # flatten
     else:
         overrides = None
-    config = parse_config(opt.config, overrides)
+    loader = ConfigFileLoader()
+    config = loader.load_config(opt.config, overrides)
 
-    train(config, rank=Rank(opt.rank))
+    train(
+        config,
+        rank=Rank(opt.rank),
+        subprocess_init=partial(add_to_sys_path, loader.config_dir.name),
+    )
 
 
 if __name__ == '__main__':

commit d0814f5442787caa6515ceb64397afbb2812ddb6

Fix config override in import_from_tsv
Summary:
This was broken because `override_config_dict` cannot handle a `None` override.

Issue introduced in af783df122f1be881052f9f4023452be980b03cc (a.k.a., D16600944).

Fixes #93.

Reviewed By: adamlerer

Differential Revision: D16936289

fbshipit-source-id: 721727a97c4ee76105298e47610db11c5cf0c3e7

torchbiggraph/converters/import_from_tsv.py
@@ -381,9 +381,7 @@ def main():
 
     if opt.param is not None:
         overrides = chain.from_iterable(opt.param)  # flatten
-    else:
-        overrides = None
-    config_dict = override_config_dict(config_dict, overrides)
+        config_dict = override_config_dict(config_dict, overrides)
 
     entity_configs, relation_configs, entity_path, dynamic_relations = \
         parse_config_partial(config_dict)