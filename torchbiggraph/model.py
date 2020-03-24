bugfix: Don't convert CPU embs (during eval) to half-precision
Summary: Eval runs the model on CPU, but there are no CPU kernels for half-precision, so make sure we run in single-precision on CPU.

Reviewed By: jiajunshen

Differential Revision: D20602871

fbshipit-source-id: efa058a0b73b3068d15503e9914096a4bab3d89f

torchbiggraph/model.py
@@ -856,7 +856,7 @@ class MultiRelationEmbedder(nn.Module):
         # 3. Prepare for the comparator.
         embs = self.comparator.prepare(embs)
 
-        if self.half_precision:
+        if self.half_precision and embs.is_cuda:
             embs = embs.half()
         return embs
 