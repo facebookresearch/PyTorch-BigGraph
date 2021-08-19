26e23ee1fe4c66c14e66ac09f2e6cf5c71c8452a
Adam Lerer <alerer@fb.com>
2021-08-19 11:12:28 -0700

    little importer bugfix
    Summary:
    fix an untested PR with another untested PR.
    
    https://github.com/facebookresearch/PyTorch-BigGraph/issues/230#issuecomment-900638212
    
    Reviewed By: lw
    
    Differential Revision: D30404532
    
    fbshipit-source-id: befa2cc64a8095780eeccb5f261d8b13802bd7b3

--- torchbiggraph/converters/importers.py
+++ torchbiggraph/converters/importers.py
@@ -66,7 +66,7 @@ class TSVEdgelistReader(EdgelistReader):
                     rel_word = words[self.rel_col] if self.rel_col is not None else None
                     weight_word = (
                         float(words[self.weight_col])
-                        if self.weight_word is not None
+                        if self.weight_col is not None
                         else None
                     )
                     yield lhs_word, rhs_word, rel_word, weight_word
