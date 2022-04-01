339ba94aa0f8fddeba7ec1e1c314e5100051d9eb
Thomas Markovich <tmarkovich@twitter.com>
2022-04-01 13:51:50 -0400

    PR Comments and typing

--- torchbiggraph/examples/sql_end2end/data_prep.py
+++ torchbiggraph/examples/sql_end2end/data_prep.py
@@ -1,4 +1,5 @@
 import argparse
+import copy
 import itertools
 import h5py
 import json
@@ -7,6 +8,8 @@ import multiprocessing as mp
 import pandas as pd
 from pathlib import Path
 import sqlite3
+from sqlite3 import Connection
+from typing import Dict, Tuple
 
 from config_template import CONFIG_TEMPLATE
 from sql_templates import (
@@ -19,13 +22,36 @@ from sql_templates import (
 import sys
 import time
 
+DEFAULT_CFG = dict(
+    # Scoring model
+    dimension=200,
+    comparator="dot",
+    loss_fn="softmax",
+    # Training
+    num_epochs=50,
+    num_uniform_negs=1000,
+    num_batch_negs=1000,
+    batch_size=150_000,
+    lr=0.05,
+    regularization_coef=1e-3,
+    num_gpus=2,
+    eval_fraction=0,
+    # io
+    checkpoint_path='model',
+    entity_path='training_data',
+    edge_paths=[
+        'training_data'
+    ],
+)
+
+
 logging.basicConfig(
     format='%(process)d-%(levelname)s-%(message)s',
     stream = sys.stdout,
     level=logging.DEBUG
 )
 
-def remap_relationships(conn):
+def remap_relationships(conn: Connection):
     """
     A function to remap relationships using SQL queries.
     """
@@ -45,7 +71,7 @@ def remap_relationships(conn):
     return rels
 
 
-def remap_entities(conn, entity2partitions):
+def remap_entities(conn: Connection, entity2partitions:  Dict[str, Dict[str, int]]):
     """
     A function to remap entities with partitioning using SQL queries.
 
@@ -59,9 +85,9 @@ def remap_entities(conn, entity2partitions):
     start=time.time()
     query = ""
     for entity, npartitions in entity2partitions.items():
-        query = QUERY_MAKE_ID2PART_TBL.format(type=entity, nparts=npartitions)
+        query = QUERY_MAKE_ID2PART_TBL.format(type=entity, nparts=npartitions['num_partitions'])
 
-        for i in range(npartitions):
+        for i in range(npartitions['num_partitions']):
             query += PARTITIONED_MAPPED_ENTITIES.format(type=entity, n=i)
         logging.debug(f"Running query: {query}")
         conn.executescript(query)
@@ -69,10 +95,10 @@ def remap_entities(conn, entity2partitions):
     logging.info(f"Remapped entities in {end - start}s")
 
 
-def generate_ctes(lhs_part, rhs_part, rels, entity2partitions):
+def generate_ctes(lhs_part: int, rhs_part: int, rels: int, entity2partitions:  Dict[str, Dict[str, int]]):
     """
-    This function generates the sub-table CTEs that help us generate
-    the completed edgelist.
+    This function generates the sub-table Common Table Expressions (CTES)
+    that help us generate the completed edgelist.
     """
     nctes = 0
     ctes = """
@@ -80,9 +106,9 @@ def generate_ctes(lhs_part, rhs_part, rels, entity2partitions):
     """
     first = True
     for _ , r in rels.iterrows():
-        if lhs_part >= entity2partitions[r['source_type']]:
+        if lhs_part >= entity2partitions[r['source_type']]['num_partitions']:
             continue
-        if rhs_part >= entity2partitions[r['destination_type']]:
+        if rhs_part >= entity2partitions[r['destination_type']]['num_partitions']:
             continue
         if not first:
             ctes += f", cte_{nctes} as ("
@@ -100,7 +126,7 @@ def generate_ctes(lhs_part, rhs_part, rels, entity2partitions):
     return nctes, ctes
 
 
-def generate_unions(nctes):
+def generate_unions(nctes: int):
     """
     This function is just a helper function for
     generating the final edge list tables.
@@ -115,7 +141,7 @@ def generate_unions(nctes):
     return subquery
 
 
-def remap_edges(conn, rels, entity2partitions):
+def remap_edges(conn: Connection, rels: pd.DataFrame, entity2partitions:  Dict[str, Dict[str, int]]):
     """
     A function to remap all edges to ordinal IDs
     according to their type.
@@ -129,7 +155,7 @@ def remap_edges(conn, rels, entity2partitions):
     """).fetchall()[0][0]
 
     query = ""
-    nparts = max(entity2partitions.values())
+    nparts = max([n['num_partitions'] for _, n in entity2partitions.items()])
     for lhs_part in range(nparts):
         for rhs_part in range(nparts):
             nctes, ctes = generate_ctes(lhs_part, rhs_part, rels, entity2partitions)
@@ -160,7 +186,7 @@ def remap_edges(conn, rels, entity2partitions):
     logging.info(f"Remapped edges in {end - start}s")
 
 
-def load_edges(fname, conn):
+def load_edges(fname: str, conn: Connection):
     """
     A simple function to load the edges into the SQL table. It is
     assumed that we will have a file of the form:
@@ -190,13 +216,15 @@ def load_edges(fname, conn):
     logging.info(f"Loading edges in {end - start}s")
 
 
-def write_relations(outdir, rels, conn):
+def write_relations(outdir: Path, rels: pd.DataFrame, conn: Connection):
     """
     A simple function to write the relevant relationship information out
     for training.
     """
     logging.info("Writing relations for training")
     start = time.time()
+
+    outdir.mkdir(parents=True, exists_ok=True)
     out = rels.sort_values('graph_id')['id'].to_list()
     with open(f'{outdir}/dynamic_rel_names.json', mode='w') as f:
         json.dump(out, f, indent=4)
@@ -204,7 +232,7 @@ def write_relations(outdir, rels, conn):
     logging.info(f"Wrote relations in {end - start}s")
 
 
-def write_single_bucket(work_packet):
+def write_single_bucket(work_packet: Tuple[int, int, Path, Connection]):
     """
     A function to write out a single edge-lists in the format that
     PyTorch BigGraph expects.
@@ -231,7 +259,7 @@ def write_single_bucket(work_packet):
             f[dset][0 : len(df)] = df[colname].tolist()
 
 
-def write_all_buckets(outdir, lhs_parts, rhs_parts, conn):
+def write_all_buckets(outdir: Path, lhs_parts: int, rhs_parts: int, conn: Connection):
     """
     A function to write out all edge-lists in the format
     that PyTorch BigGraph expects.
@@ -249,15 +277,19 @@ def write_all_buckets(outdir, lhs_parts, rhs_parts, conn):
     logging.info(f"Wrote edges in {end - start}s")
     
 
-def write_entities(outdir, entity2partitions, conn):
+def write_entities(
+    outdir: Path,
+    entity2partitions:  Dict[str, Dict[str, int]],
+    conn: Connection):
     """
     A function to write out all of the training relevant
     entity information that PyTorch BigGraph expects
     """
     logging.info("Writing entites for training")
     start = time.time()
+
     for entity_type, nparts in entity2partitions.items():
-        for i in range(nparts):
+        for i in range(nparts['num_partitions']):
             query = f"""
                 select count(*)
                 from tmp_{entity_type}_ids_map_{i}
@@ -269,60 +301,64 @@ def write_entities(outdir, entity2partitions, conn):
     logging.info(f"Wrote entites in {end - start}s")
 
 
-def write_training_data(outdir, rels, entity2partitions, conn):
+def write_training_data(
+    outdir: Path,
+    rels: pd.DataFrame,
+    entity2partitions:  Dict[str, Dict[str, int]],
+    conn: Connection
+    ):
     """
     A function to write out all of the training relevant
     information that PyTorch BigGraph expects
     """
     lhs_parts = 1
     rhs_parts = 1
-    for i, r in rels.iterrows():
-        if entity2partitions[r['source_type']] > lhs_parts:
+    for _, r in rels.iterrows():
+        if entity2partitions[r['source_type']]['num_partitions'] > lhs_parts:
             lhs_parts = entity2partitions[r['source_type']]
-        if entity2partitions[r['destination_type']] > rhs_parts:
+        if entity2partitions[r['destination_type']]['num_partitions'] > rhs_parts:
             rhs_parts = entity2partitions[r['destination_type']]
 
-    # write_relations(outdir, rels, conn)
-    # write_all_buckets(rels, lhs_parts, rhs_parts, conn)
+    write_relations(outdir, rels, conn)
+    write_all_buckets(rels, lhs_parts, rhs_parts, conn)
     write_entities(outdir, entity2partitions, conn)
 
 
-def write_rels_dict(rels):
-  my_rels = ""
-  for _, row in rels.sort_values(by="graph_id").iterrows():
-    r = "{"
-    r += f"'name': '{row['id']}', 'lhs': '{row['source_type']}', 'rhs': '{row['destination_type']}', 'operator': 'translation'"
-    r += "},\n"
-    my_rels += r
-  return my_rels
+def write_config(
+    rels: pd.DataFrame,
+    entity2partitions: Dict[str, Dict[str, int]],
+    config_out: Path,
+    train_out: Path,
+    model_out: Path,
+    ndim: int = 200,
+    ngpus:int = 2
+    ):
+    config_out.mkdir(parents=True, exists_ok=True)
+    outname = config_out / 'config.py'
+    rels['operator'] = 'translation'
+    rels = rels.rename({'id': 'name', 'source_type': 'lhs', 'destination_type': 'rhs'}, axis=1)
 
+    cfg = copy.deepcopy(DEFAULT_CFG)
+    cfg['edge_paths'] = [ train_out ]
+    cfg['entity_path'] = train_out
+    cfg['checkpoint_path'] = model_out
+    cfg['entites'] = entity2partitions
+    cfg['relations'] = rels[['name', 'lhs', 'rhs', 'operator']].to_dict(orient='records')
 
-def write_entities_dict(entity2partitions):
-    my_entities = "{\n"
-    for name, part in entity2partitions.items():
-        my_entities += '\t"{name}": {{"num_partitions": {part} }},\n'.format(name=name, part=part)
-    my_entities += "}\n"
-    return my_entities
-
-
-def write_config(rels, entity2partitions, config_out, train_out, model_out):
-    outname = Path(config_out) / 'config.py'
     with open(outname, mode='w') as f:
         f.write(
-            CONFIG_TEMPLATE.format(
-                RELN_DICT=write_rels_dict(rels),
-                ENTITIES_DICT=write_entities_dict(entity2partitions),
-                TRAINING_DIR=train_out,
-                MODEL_PATH=model_out,
-            )
+            f"def get_torchbiggraph_config():\n\treturn {json.dumps(cfg, indent=4)}\n"
         )
 
 
-def compute_memory_usage(entity2partitions, conn, NDIM=200):
+def compute_memory_usage(
+    entity2partitions: Dict[str, Dict[str, int]],
+    conn: Connection,
+    ndim: int):
     nentities = 0
     for _type, parts in entity2partitions.items():
         ntype = 0
-        for i in range(parts):
+        for i in range(parts['num_partitions']):
             query = f"""
                 select count(*) as cnt
                 from `tmp_{_type}_ids_map_{i}`
@@ -330,41 +366,52 @@ def compute_memory_usage(entity2partitions, conn, NDIM=200):
             ntype = max(ntype, conn.executequery(query).fetchall()[0][0])
         nentities += ntype
 
-    mem = 1.5 * nentities * NDIM * 8 / 1024 / 1024 / 1024
-    logging.info(f"I need {mem} GBs of ram for embedding table for {NDIM} Dimensions")
+    # 1.2 here is an empirical safety factor.
+    mem = 1.2 * nentities * ndim * 8 / 1024 / 1024 / 1024
+    logging.info(f"I need {mem} GBs of ram for embedding table for {ndim} Dimensions")
 
 
-def main(nparts=2, edge_file_name='edges.csv', outdir='training_data/', modeldir='model/', config_dir=''):
-    conn = sqlite3.connect("citationv2.db")
-    load_edges(edge_file_name, conn)
+def main(
+    nparts: int = 1,
+    edge_file_name: str = 'edges.csv',
+    outdir: Path = Path('training_data/'),
+    modeldir: Path = Path('model/'),
+    config_dir: Path = Path(''),
+    dbname: str = 'citationv2.db') -> None:
+    conn = sqlite3.connect(dbname)
+    # load_edges(edge_file_name, conn)
 
     entity2partitions = {
-        'paper': nparts,
-        'year': 1,
+        'paper': {'num_partitions': nparts},
+        'year': {'num_partitions': 1},
     }
 
     rels = remap_relationships(conn)
     remap_entities(conn, entity2partitions)
     remap_edges(conn, rels, entity2partitions)
-    Path(outdir).mkdir(parents=True, exist_ok=True)
-    out = Path(outdir)
-    write_training_data(out, rels, entity2partitions, conn)
-    write_config(rels, entity2partitions, config_dir, out, modeldir)
+
+    outdir.mkdir(parents=True, exists_ok=True)
+    config_dir.mkdir(parents=True, exists_ok=True)
+    modeldir.mkdir(parents=True, exists_ok=True)
+
+    write_training_data(outdir, rels, entity2partitions, conn)
+    write_config(rels, entity2partitions, config_dir, outdir, modeldir)
+    compute_memory_usage(entity2partitions, conn, 200)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("npart", help="The number of partitions to split the paper_ids into")
-    parser.add_argument("e", help="The edges file to load in")
-    parser.add_argument("o", help="The directory where the training data should be stored")
-    parser.add_argument("m", help="The directory where the model artifacts should be stored")
-    parser.add_argument("c", help="The location where the generated config file will be stored")
+    parser.add_argument("-npart", help="The number of partitions to split the paper_ids into", type=int)
+    parser.add_argument("-e", help="The edges file to load in")
+    parser.add_argument("-o", help="The directory where the training data should be stored", default='training_data/', required=False)
+    parser.add_argument("-m", help="The directory where the model artifacts should be stored", default='model', required=False)
+    parser.add_argument("-c", help="The location where the generated config file will be stored", default='.', required=False)
     opt = parser.parse_args()
 
     main(
         nparts=opt.npart,
         edge_file_name=opt.e,
-        outdir=opt.o,
-        modeldir=opt.m,
-        config_dir=opt.c
+        outdir=Path(opt.o),
+        modeldir=Path(opt.m),
+        config_dir=Path(opt.c),
     )
